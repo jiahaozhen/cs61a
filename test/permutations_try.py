c7109a340f39003af355459f6596fe06a615e88a
Jia <jiahaozhen@foxmail.com>
2022-11-23 20:02:01 +0800

    hw04 done

---
+++ test/permutations_try.py
@@ -0,0 +1,27 @@
+def permutations(seq):
+    """
+    >>> perms = permutations([100])
+    >>> type(perms)
+    <class 'generator'>
+    >>> next(perms)
+    [100]
+    >>> try: #this piece of code prints "No more permutations!" if calling next would cause an error
+    ...     next(perms)
+    ... except StopIteration:
+    ...     print('No more permutations!')
+    No more permutations!
+    >>> sorted(permutations([1, 2, 3])) # Returns a sorted list containing elements of the generator
+    [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
+    >>> sorted(permutations((10, 20, 30)))
+    [[10, 20, 30], [10, 30, 20], [20, 10, 30], [20, 30, 10], [30, 10, 20], [30, 20, 10]]
+    >>> sorted(permutations("ab"))
+    [['a', 'b'], ['b', 'a']]
+    """
+    if len(seq) == 1:
+        yield list(seq)
+    else:
+        for sub_permutation in permutations(seq[1:]):
+            for index in range(len(seq)):
+                result = sub_permutation[:]
+                result.insert(index, seq[0])
+                yield result
