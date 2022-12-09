143485166cf28130ce3a0d3e309b852bfe547666
Jia <jiahaozhen@foxmail.com>
2022-12-09 17:28:14 +0800

    hw06 done

--- homework/hw06/hw06.scm
+++ homework/hw06/hw06.scm
@@ -3,15 +3,20 @@
 
 (define (cadr s)
   'YOUR-CODE-HERE
+  (car (cdr s))
 )
 
 (define (caddr s)
   'YOUR-CODE-HERE
+  (car (cddr s))
 )
 
 
 (define (sign num)
   'YOUR-CODE-HERE
+  (cond ((< num 0) -1)
+    ((= num 0) 0)
+    ((> num 0) 1))
 )
 
 
@@ -19,5 +24,9 @@
 
 (define (pow x y)
   'YOUR-CODE-HERE
+  (cond ((= x 1) 1)
+        ((= y 1) x)
+        ((even? y) (square (pow x (/ y 2)))) 
+        ((odd? y) (* x (pow x (- y 1)))))
 )
 
