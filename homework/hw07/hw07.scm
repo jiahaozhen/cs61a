876a787ece3db060f570b0eae23e5fa6ec9a066d
Jia <jiahaozhen@foxmail.com>
2023-01-26 22:41:43 +0800

    hw07 done

--- homework/hw07/hw07.scm
+++ homework/hw07/hw07.scm
@@ -1,5 +1,10 @@
 (define (filter-lst fn lst)
   'YOUR-CODE-HERE
+  (if (null? lst)
+    ()
+    (if (fn (car lst))
+      (cons (car lst) (filter-lst fn (cdr lst)))
+      (filter-lst fn (cdr lst))))
 )
 
 ;;; Tests
@@ -11,6 +16,11 @@
 
 (define (interleave first second)
   'YOUR-CODE-HERE
+  (if (null? first)
+    second
+    (cons (car first) (interleave second (cdr first)))
+  )
+  
 )
 
 (interleave (list 1 3 5) (list 2 4 6))
@@ -25,10 +35,18 @@
 
 (define (accumulate combiner start n term)
   'YOUR-CODE-HERE
+  (if (= n 1)
+    (combiner start (term n))
+    (combiner (term n) (accumulate combiner start (- n 1) term)))
 )
 
 
 (define (no-repeats lst)
   'YOUR-CODE-HERE
+  (if (null? lst)
+    ()
+    (cons (car lst) 
+      (no-repeats 
+        (filter-lst (lambda (x) (not (= x (car lst)))) (cdr lst)))))
 )
 
